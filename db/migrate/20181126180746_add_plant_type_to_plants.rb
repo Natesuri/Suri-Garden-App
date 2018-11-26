0757c3c30ac999ac13af39b3f7b218e8b19be676
Natesuri <natesuri@gmail.com>
2018-11-26 13:09:26 -0500

    added plant_type to replace invalid type column

---
+++ db/migrate/20181126180746_add_plant_type_to_plants.rb
@@ -0,0 +1,5 @@
+class AddPlantTypeToPlants < ActiveRecord::Migration[5.2]
+  def change
+    add_column :plants, :plant_type, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_170122) do
+ActiveRecord::Schema.define(version: 2018_11_26_180746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2018_11_26_170122) do
     t.string "ideal_brightness"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "plant_type"
   end
 
   create_table "plots", force: :cascade do |t|
