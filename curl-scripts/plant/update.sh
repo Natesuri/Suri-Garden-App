ab441cf5a2b20b970cf58c7c57be69abc79d3d15
Natesuri <natesuri@gmail.com>
2018-11-26 13:31:57 -0500

    Created CRUD Curl scripts for Plants Table

--- curl-scripts/plant/update.sh
+++ curl-scripts/plant/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl "http://localhost:4741/plants" \
+curl "http://localhost:4741/plants/${ID}" \
   --include \
-  --request POST \
+  --request PATCH \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
